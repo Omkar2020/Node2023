36299e283f9bbcbe306a076de400c9ef996b5740
Omkar2020 <omkar.pattajoshi@yahoo.com>
2023-07-12 06:35:37 +0530

    Second Commit

--- nodeApp.js
+++ nodeApp.js
@@ -1,1 +1,10 @@
+
+const os = require('os');
 console.log("Hi I am Test nodeApp")
+
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log(`Total Memory : ${totalMemory}`);
+console.log(`Free Memory : ${freeMemory}`);
